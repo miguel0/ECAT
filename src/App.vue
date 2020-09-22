c66b96ee6581a38f2bfc05bcfc5133790383efee
miguel0 <miguelbarroe@gmail.com>
2020-09-22 16:43:15 -0700

    initial sign in

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-	<b-container>
-		<p>Hello, Bootstrap-Vue</p>
-	</b-container>
+	<div id="app">
+		<SignIn />
+	</div>
 </template>
+
+<script>
+import SignIn from "./views/SignIn.vue"
+
+export default {
+	name: "app",
+	components: {
+		SignIn
+	}
+}
+</script>
