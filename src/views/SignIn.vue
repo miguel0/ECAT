47e55e2caa36dec64cec2f7a911b86a23684eb40
miguel0 <miguelbarroe@gmail.com>
2020-09-22 19:03:40 -0700

    log in complete

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,75 +1,56 @@
 <template>
-	<div id="sign-in">
-		<form class="form-signin">
-			<img class="mb-4" src="../assets/logo_at-motors.png" alt="" width="72" height="72">
-			<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-			<label for="inputEmail" class="sr-only">Email address</label>
-			<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-			<label for="inputPassword" class="sr-only">Password</label>
-			<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-			<div class="checkbox mb-3">
-				<label>
-					<input type="checkbox" value="remember-me"> Remember me
-				</label>
+	<div id="signin">
+		<form class="form-signin text-center">
+			<img id="logo" class="mt-5 img-fluid" src="../assets/logo_at-motors.png" alt="" width="200%" height="auto">
+			<div class="form">
+				<label for="inputEmail" class="sr-only">Correo electrónico</label>
+				<input type="email" id="inputEmail" class="form-control" placeholder="Correo electrónico" required autofocus>
+				<label for="inputPassword" class="sr-only">Contraseña</label>
+				<input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+				<button class="btn btn-lg btn-primary btn-block mt-3 mb-3" type="submit">Iniciar Sesión</button>
+				<a href="#">¿Olvidaste tu contraseña?</a>
 			</div>
-			<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-			<p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
 		</form>
 	</div>
 </template>
 
 <script>
 export default {
-	name: "SignIn",
-	props: {
-
-	}
+	name: "SignIn"
 }
 </script>
 
 <style scoped>
-	html,
-body {
-  height: 100%;
+#signin {
+	display: flex;
 }
-
-body {
-  display: -ms-flexbox;
-  display: flex;
-  -ms-flex-align: center;
-  align-items: center;
-  padding-top: 40px;
-  padding-bottom: 40px;
-  background-color: #f5f5f5;
+.form {
+	padding-top: 40%;
 }
-
 .form-signin {
-  width: 100%;
-  max-width: 330px;
-  padding: 15px;
-  margin: auto;
-}
-.form-signin .checkbox {
-  font-weight: 400;
+	width: 100%;
+	max-width: 330px;
+	padding: 15px;
+	margin: auto;
 }
 .form-signin .form-control {
-  position: relative;
-  box-sizing: border-box;
-  height: auto;
-  padding: 10px;
-  font-size: 16px;
+	position: relative;
+	box-sizing: border-box;
+	height: auto;
+	padding: 10px;
+	font-size: 16px;
 }
 .form-signin .form-control:focus {
-  z-index: 2;
+	z-index: 2;
 }
 .form-signin input[type="email"] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
+	margin-bottom: -1px;
+	border-bottom-right-radius: 0;
+	border-bottom-left-radius: 0;
 }
 .form-signin input[type="password"] {
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
+	margin-bottom: 10px;
+	border-top-left-radius: 0;
+	border-top-right-radius: 0;
 }
 </style>
