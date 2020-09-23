0714a5d0b52faee3cc0116acacd1db34ba939863
luiswislo <luisfwilson1998@gmail.com>
2020-09-23 16:44:23 -0700

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,10 +3,10 @@
         <b-navbar type='dark' variant='primary'>
             <b-navbar-brand href="#">ecat</b-navbar-brand>
             <b-navbar-nav class='ml-auto'>
-                <template v-for='section in sections'>
-                    <b-nav-item v-if='!section.children' :key='section.key' :href=section.route>{{section.name}}</b-nav-item>
-                    <b-nav-item-dropdown v-else :key='section.key' :text="section.name" right>
-                        <b-dropdown-item v-for='child in section.children' :key='child.key' :href='child.route'>{{child.name}}</b-dropdown-item>
+                <template v-for='(section, index) in sections'>
+                    <b-nav-item v-if='!section.children' :key="`section-${index}`" :href=section.route>{{section.name}}</b-nav-item>
+                    <b-nav-item-dropdown v-else :key="`section-${index}`" :text="section.name" right>
+                        <b-dropdown-item v-for='(child, i) in section.children' :key="`child-${i}`" :href='child.route'>{{child.name}}</b-dropdown-item>
                     </b-nav-item-dropdown>
                 </template>
             </b-navbar-nav>
@@ -22,38 +22,31 @@ export default {
             sections: [
                 {
                     route: '/catalog',
-                    name: 'Catálogos',
-                    key: 'catalog'
+                    name: 'Catálogos'
                 },
                 {
                     name: 'Administración',
-                    key: 'admin',
                     children: [
                         {
                             route: '/admin/datos',
-                            name: 'Agregar Datos',
-                            key: 'data'
+                            name: 'Agregar Datos'
                         },
                         {
                             route: '/admin/usuarios',
-                            name: 'Manejo de Usuarios',
-                            key: 'users'
+                            name: 'Manejo de Usuarios'
                         }
                     ]
                 },
                 {
                     name: 'Manuel Córdoba', // TODO: change, hardcoded name,
-                    key: 'user',
                     children: [
                         {
                             route: '/contraseña',
                             name: 'Cambiar Contraseña',
-                            key: 'password'
                         },
                         {
                             route: '/',
                             name: 'Cerrar Sesión',
-                            key: 'logout'
                         }
                     ]
                 }
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,11 +1,21 @@
 <template>
     <div>
-        <!--<Navbar/>-->
+        <Navbar/>
         <br>
         <b-container>
             <b-row>
                 <b-col>
                     <b-form-input :placeholder="searchPlaceHolder" type='search'></b-form-input>
+                    <br>
+                    <b-list-group>
+                        <b-list-group-item v-for="(vehicle, index) in sampleVehicleList" :key="index" class="flex-column align-items-start">
+                            <div>
+                                <h5>
+                                    
+                                </h5>
+                            </div>
+                        </b-list-group-item>
+                    </b-list-group>
                 </b-col>
             </b-row>
         </b-container>
@@ -16,16 +26,22 @@
 
 <script>
 
-//import Navbar from '../components/Navbar' 
+import Navbar from '../components/Navbar' 
 
 export default {
     name: 'Catalog',
     components: {
-        //Navbar
+        Navbar
     },
     data() {
         return {
-            searchPlaceHolder: 'Busca por VIN'
+            searchPlaceHolder: 'Busca por VIN',
+            sampleVehicleList: [
+                {
+                    name: 'TRACTOR C7H',
+                    vin: 'CYMS18010001-C7H 6X2 MT13.43-50 ZF16S2231TO HR7 MCY13Q(4.11)'
+                }
+            ]
         }
     }
 }
