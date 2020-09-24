8f129afe404d62749db15dc61b87af4624a6ef61
luiswislo <luisfwilson1998@gmail.com>
2020-09-23 18:35:54 -0700

    filters

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,21 +1,34 @@
 <template>
     <div>
         <Navbar/>
-        <br>
+        <br> 
         <b-container>
             <b-row>
                 <b-col>
                     <b-form-input :placeholder="searchPlaceHolder" type='search'></b-form-input>
+                    <br><br>
+                    <h5>Buscando</h5>
                     <br>
-                    <b-list-group>
-                        <b-list-group-item v-for="(vehicle, index) in sampleVehicleList" :key="index" class="flex-column align-items-start">
-                            <div>
-                                <h5>
-                                    
-                                </h5>
-                            </div>
-                        </b-list-group-item>
+                    <b-list-group horizontal>
+                        <b-list-group-item v-for="(filter, index) of sampleFilters" :key="`filter-${index}`" >{{filter}}</b-list-group-item>
                     </b-list-group>
+                    <br><br>
+                    <h4>Resultados encontrados ({{sampleVehicleList.length}})</h4>
+                    <br>
+                    <b-card-group>
+                        <b-card no-body v-for="(vehicle, index) in sampleVehicleList" :key="index" img-left class="overflow-hidden">
+                            <b-row no-gutters>
+                                <b-card-img :src="sampleImg" class="thumb"></b-card-img>
+                                <b-col>
+                                    <b-card-body :title="vehicle.name">
+                                        <b-card-text class="text-secondary">
+                                            {{vehicle.vin}}
+                                        </b-card-text>
+                                    </b-card-body>
+                                </b-col>
+                            </b-row>
+                        </b-card>
+                    </b-card-group>
                 </b-col>
             </b-row>
         </b-container>
@@ -41,16 +54,23 @@ export default {
                     name: 'TRACTOR C7H',
                     vin: 'CYMS18010001-C7H 6X2 MT13.43-50 ZF16S2231TO HR7 MCY13Q(4.11)'
                 }
+            ],
+            //sampleImg: '../assets/img/test/truck.png'
+            sampleImg: 'https://placekitten.com/300/300',
+            sampleFilters: [
+                'Vehículos',
+                'Componentes',
+                'Piezas'
             ]
         }
     }
 }
 </script>
+
 <style scoped>
-.back {
-    background-color: red;
+.thumb {
+    height: 150px;
+    width: 150px;
 }
-.main {
-    max-width: 70%;
-}
+
 </style>
