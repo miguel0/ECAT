e90da7000f72a5c6b9fdb91f814378a109d3e1c1
miguel0 <miguelbarroe@gmail.com>
2020-10-22 09:48:19 -0700

    [home] created truck search page

---
+++ src/views/TruckSearch.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<Navbar />
+	</div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar';
+
+export default {
+	name: 'TruckSearch',
+	components: {
+		Navbar
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
