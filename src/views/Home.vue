a0830c54d67973ff4054828e94ce44ff98b840c7
Victor A. Zazueta <A00226702@itesm.mx>
2020-10-22 20:30:03 -0700

    [Home] Margenes de Botones (1)
    Signed-off-by: Victor A. Zazueta <A00226702@itesm.mx>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,36 +4,38 @@
 		
         <b-card-group id="search-buttons" deck class="p-3">
             
-            <b-card 
-            id="vehicle-search"
-            img-alt="Card image" 
-            class="text-center position-relative p-2"
-            img-top
-            @click="goToVehicle()">
-                <b-card-img 
-                :src="require('../assets/img/test/vehicle-default.png')">
-                </b-card-img>
+            <div class="search-btn">
+                <b-card 
+                img-alt="Card image" 
+                class="text-center position-relative p-2"
+                img-top
+                @click="goToVehicle()">
+                    <b-card-img 
+                    :src="require('../assets/img/test/vehicle-default.png')">
+                    </b-card-img>
 
-                <b-card-text>
-                Búsqueda por Vehículos
-                </b-card-text>
-            </b-card>
+                    <b-card-text>
+                    Búsqueda por Vehículos
+                    </b-card-text>
+                </b-card>
+            </div>
 
-            <b-card 
-            id="individual-search"
-            img-alt="Card image" 
-            class="text-center position-relative p-2"
-            img-top>
-                <b-card-img 
-                :src="require('../assets/img/test/individual-parts-default.png')">
-                </b-card-img>
+            <div class="search-btn">
+                <b-card 
+                id="individual-search"
+                img-alt="Card image" 
+                class="text-center position-relative p-2"
+                img-top>
+                    <b-card-img 
+                    :src="require('../assets/img/test/individual-parts-default.png')">
+                    </b-card-img>
 
-                <b-card-text>
-                Búsqueda por Partes
-                </b-card-text>
-            </b-card>
+                    <b-card-text>
+                    Búsqueda por Partes
+                    </b-card-text>
+                </b-card>
+            </div>
         </b-card-group>
-        
 		
     </div>
     
@@ -64,30 +66,21 @@
         display: flex;
         max-width: 800px;
         width: 75%;
-        height: 100%;
+        height: 75%;
         margin: auto;
         align-items: center;
         justify-content: center;
-
+        
     }
 
     #page {
         height: 80vh;
     }
 
-    b-card {
-        background: darkgray;
+    .search-btn {
+        cursor: pointer;
+        width: 50%;
         
     }
-
-    b-card-img {
-        width: 30%;
-        height: 30%;
-    }
-
-    b-card-text {
-
-    }
-
 
 </style>
