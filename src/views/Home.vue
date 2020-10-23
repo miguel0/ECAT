f19f555fe7a2fd87f8f4c5e331027f8b4f50914e
Victor A. Zazueta <A00226702@itesm.mx>
2020-10-22 19:43:54 -0700

    [Home] - Implement Buttons and Vehicle Search
    Signed-off-by: Victor A. Zazueta <A00226702@itesm.mx>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,40 @@
 <template>
-    <div>
+    <div id="page">
         <Navbar/>
-		<div>hello world</div>
+		
+        <b-card-group id="search-buttons" deck class="p-3">
+            
+            <b-card 
+            id="vehicle-search"
+            img-alt="Card image" 
+            class="text-center position-relative p-2"
+            img-top
+            @click="goToVehicle()">
+                <b-card-img 
+                :src="require('../assets/img/test/vehicle-default.png')">
+                </b-card-img>
+
+                <b-card-text>
+                Búsqueda por Vehículos
+                </b-card-text>
+            </b-card>
+
+            <b-card 
+            id="individual-search"
+            img-alt="Card image" 
+            class="text-center position-relative p-2"
+            img-top>
+                <b-card-img 
+                :src="require('../assets/img/test/individual-parts-default.png')">
+                </b-card-img>
+
+                <b-card-text>
+                Búsqueda por Partes
+                </b-card-text>
+            </b-card>
+        </b-card-group>
+        
+		
     </div>
     
 </template>
@@ -13,6 +46,48 @@
         name: 'Home',
         components: {
             Navbar
+        },
+
+        methods:{
+
+            goToVehicle(){
+                location.href = "/trucksearch";
+            }
+
         }
     }
 </script>
+
+<style scoped>
+
+    #search-buttons {
+        display: flex;
+        max-width: 800px;
+        width: 75%;
+        height: 100%;
+        margin: auto;
+        align-items: center;
+        justify-content: center;
+
+    }
+
+    #page {
+        height: 80vh;
+    }
+
+    b-card {
+        background: darkgray;
+        
+    }
+
+    b-card-img {
+        width: 30%;
+        height: 30%;
+    }
+
+    b-card-text {
+
+    }
+
+
+</style>
