f66a3c66245a4d54be6380581b403efb4ca65de8
FgRogelio <rfelix@owd.mx>
2020-11-05 21:33:57 -0700

    [edit-part][edit-vehicle] Implemented vehicle editing and set a standard style for both views

--- src/services/api/src/vehicles.api.js
+++ src/services/api/src/vehicles.api.js
@@ -7,5 +7,17 @@ export default {
     },
     getAllVehicles: () => {
         return axios.get('/vehicles');
-    }
+    },
+	editVehicle: (id, name, spName, otherName, model, type, motorConfig, motorPower, transmission) => {
+		return axios.put(`/vehicles/${id}`, {
+			name: name,
+			spName: spName,
+            otherName: otherName,
+            model: model,
+            type: type,
+            motorConfig: motorConfig,
+            motorPower: motorPower,
+            transmission: transmission
+		})
+	}
 }
