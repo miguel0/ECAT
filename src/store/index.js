924b101b07bef4111a87e4c85ea9671c35d8802e
miguel0 <miguelbarroe@gmail.com>
2020-11-12 13:33:23 -0700

    [front-auth] logout if user not in db

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import * as fb from '../firebase';
 import router from '../router/index';
+import api from '../services/api/api';
 
 Vue.use(Vuex);
 
@@ -23,31 +24,41 @@ const store = new Vuex.Store({
 			dispatch('fetchUserProfile', user);
 		},
 		async fetchUserProfile({ commit }, user) {
-			// fetch user profile
-			const userProfile = user; // TODO: get user from db
+			if (!user.uid) {
+				logout(commit);
+			} else {
+				// fetch user profile
+				let canContactServer = false;
+				await api.usersApi.getUser(user.uid).then(data => {
+					canContactServer = true;
 
-			// set user profile in state
-			commit('setUserProfile', userProfile);
-			
-			// change route to dashboard
-			if (router.currentRoute.path === '/') {
-				router.push('/home')
+					if (!data.id) {
+						logout(commit);
+					} else {
+						// set user profile state
+						commit('setUserProfile', data);
+
+						// change route to dashboard
+						if (router.currentRoute.path === '/') {
+							router.push('/home')
+						}
+					}
+				}).catch(err => {
+					console.log(err);
+				});
+
+				if (!canContactServer) {
+					logout(commit);
+				}
 			}
 		},
 		async logout({ commit }) {
-			// log user out
-			await fb.auth.signOut();
-
-			// clear user data from state
-			commit('setUserProfile', {});
-
-			// redirect to login view
-			router.push('/');
+			logout(commit);
 		},
 		async changePW({ dispatch }, form) {
 			fb.auth.sendPasswordResetEmail(form.email).then(function() {
 				// Email sent.
-				alert('¡Se ha enviado un correo para cambiar su contraseña!')
+				alert('¡Se ha enviado un correo para cambiar su contraseña!');
 				dispatch('logout');
 			}).catch(function(error) {
 				// An error happened.
@@ -56,5 +67,18 @@ const store = new Vuex.Store({
 		}
 	}
 });
+
+async function logout(commit) {
+	// log user out
+	await fb.auth.signOut();
+
+	// clear user data from state
+	commit('setUserProfile', {});
+
+	// redirect to login view
+	if (router.currentRoute.path != '/') {
+		router.push('/')
+	}
+}
 
 export default store;
