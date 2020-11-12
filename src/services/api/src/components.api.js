babb3e9cac137517c5dffb9496c5e2b479328816
Victor Alfonso Zazueta <42273182+vzazueta@users.noreply.github.com>
2020-11-11 19:12:00 -0700

    Revert "Revert "Edit component""

--- src/services/api/src/components.api.js
+++ src/services/api/src/components.api.js
@@ -5,8 +5,20 @@ export default {
     getAllComponents: () => {
         return axios.get('/users');
     },
+    
     getComponent: (id) => {
         return axios.get(`/components/${id}`);
-    }
+    },
+	
+	editComponent: (id, name, chName, spName, otherName) => {
+		return axios.put(`/components/${id}`, {
+			name: name,
+			chName: chName,
+			spName: spName,
+			otherName: otherName,
+		})
+			.then(res => { return res.data })
+			.catch(err => { throw err.response.data })
+	}
 
 } 
